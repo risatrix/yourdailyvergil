3ff874489163d15d52e1446e93755717b0c7c0cc
Amanda <amanda.n.krauss@gmail.com>
2014-12-28 13:54:40 -0600

    make English translation conditional on 200 status; add book-changing functionality

--- bot.js
+++ bot.js
@@ -16,44 +16,51 @@ var latin = "";
 
 function makeVergil() {
     line_counter += 1;
-    // // There are only 12 books in the Aeneid
-    // if (book_counter <= 11) {
-    //     book_counter +=1;
-    // }
-    // else {
-    //     //reset entirely if we get past the last book
-    //     book_counter = 1;
-    //     line_counter = 1;
-    // }
+
     var getLatinURL = LatinURLBase + '/' + book_counter + '/' + line_counter;
     var getEnglishURL = EnglishURLBase + '/' + book_counter + '/' + line_counter;
+
 	request(getLatinURL,
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body);
-    	       latin = (data.text);
-  		    }
-            else {
-                console.log('Status Code: ' + response.statusCode);
-                var error_msg = (JSON.parse(body)).error;
-                console.log('Error:' + error_msg);
-            }
-        request(getEnglishURL,
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
                 var data = JSON.parse(body);
-                english = (data.text);
+                latin = (data.text);
+                //only request the English trans if the Latin returned okay
+                request(getEnglishURL,
+                    function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var data = JSON.parse(body);
+                            english = (data.text);
+                        }
+                        else {
+                            //Less informative because any line number errors
+                            //should be taken care of by the first if stmt
+                            console.log('Status Code: ' + response.statusCode);
+                        }
+                        //only proceed with output if status is okay
+                        statement = latin + '/ ' + english;
+                        console.log('book ' + book_counter + ', line ' + line_counter);
+                        console.log(statement);
+  		        });
             }
             else {
                 console.log('Status Code: ' + response.statusCode);
                 var error_msg = (JSON.parse(body)).error;
                 console.log('Error:' + error_msg);
+                //Because it's just a bot, wer'e going to assume that an error
+                //means we've run out of lines in the book
+                console.log('Changing books...');
+                // There are only 12 books in the Aeneid, so...
+                if (book_counter <= 11) {
+                    book_counter +=1;
+                }
+                else {
+                    //Go back to first book if we've finished 12
+                    book_counter = 1;
+                }
+                line_counter = 0;
             }
-        statement = latin + '/ ' + english;
-        console.log('book ' + book_counter + ', line ' + line_counter);
-        console.log(statement);
         });
-    });
 }
 
 // Try to retweet something as soon as we run the program...
