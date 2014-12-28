d4c2c0b5294d2aea4b5459cf32d6aa83be5adc72
Amanda <amanda.n.krauss@gmail.com>
2014-12-27 20:30:47 -0600

    combine english and latin lines
    Conflicts:
    	bot.js

--- bot.js
+++ bot.js
@@ -4,38 +4,42 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+// URLs to create Vergil Tweets
+var LatinURLBase = "http://api.aeneid.eu/versions/latin/";
+var EnglishURLBase = "http://api.aeneid.eu/versions/dryden/";
 
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+var book_counter = 0;
+var line_counter = 0;
+
+var english = "";
+var latin = "";
+
+function makeVergil() {
+    line_counter += 1;
+    book_counter +=1;
+    getLatinURL = LatinURLBase + '/' + book_counter + '/' + line_counter;
+    getEnglishURL = EnglishURLBase + '/' + book_counter + '/' + line_counter;
+	request(getLatinURL,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+    	       latin = (data.text);
+  		}
+        request(getEnglishURL,
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                var data = JSON.parse(body);
+                english = (data.text);
+                // console.log(english);
+            }
+        statement = latin + '/ ' + english;
+        console.log(statement);
+        });
+    });
 }
 
 // Try to retweet something as soon as we run the program...
-retweetLatest();
+makeVergil();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+setInterval(makeVergil(), 1000 * 60 * 60);
