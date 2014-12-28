19916183f000e55c78610b856178d04851d51812
Amanda <amanda.n.krauss@gmail.com>
2014-12-28 17:29:14 -0600

    set timer to correct interval; whitespace and comments
    Conflicts:
    	bot.js

--- bot.js
+++ bot.js
@@ -25,14 +25,14 @@ var book_counter = 1;
 var line_counter = 0;
 
 function makeVergilTweet() {
-    //increment the line number
+    // increment the line number
     line_counter += 1;
 
-    //build urls for the new line
+    // build urls for the new line
     var getLatinURL = LatinURLBase + '/' + book_counter + '/' + line_counter;
     var getEnglishURL = EnglishURLBase + '/' + book_counter + '/' + line_counter;
 
-    //use the Node request module to visit the api page urls
+    // use the Node request module to visit the api page urls
 	request(getLatinURL,
         function (error, response, body) {
             // if the URL returns info and the status is okay
@@ -40,7 +40,7 @@ function makeVergilTweet() {
             if (!error && response.statusCode == 200) {
                 var data = JSON.parse(body);
                 latin = (data.text);
-                //we only request the English URL if the Latin returned okay
+                // we only request the English URL if the Latin returned okay
                 request(getEnglishURL,
                     function (error, response, body) {
                         if (!error && response.statusCode == 200) {
@@ -48,15 +48,15 @@ function makeVergilTweet() {
                             english = (data.text);
                         }
                         else {
-                            //Less informative because any line number errors
-                            //should be taken care of by the first if stmt
+                            // Less informative because any line number errors
+                            // should be taken care of by the first if stmt
                             console.log('Status Code: ' + response.statusCode);
                         }
-                        //builds the Tweet
+                        // builds the Tweet
                         statement = latin + '/ ' + english;
                         console.log('book ' + book_counter + ', line ' + line_counter);
                         console.log(statement);
-                        //Tweets the Tweet
+                        // Tweets the Tweet
                         // T.post('statuses/update', { status: exclamation},
                         //     function(err, reply) {
                         //         console.log("error: " + err);
@@ -64,23 +64,23 @@ function makeVergilTweet() {
                         // });
   		        });
             }
-            //if the header returns anything other than 200 (=okay),
-            //we won't build anything.
+            // if the header returns anything other than 200 (=okay),
+            // we won't build anything.
             else {
-                //Print out the status and error message so we know what's going on.
+                // Print out the status and error message so we know what's going on.
                 console.log('Status Code: ' + response.statusCode);
                 var error_msg = (JSON.parse(body)).error;
                 console.log('Error:' + error_msg);
-                //In this context, most errors will be 404s that mean
-                //we've run out of lines in the book, so we'll trigger a switch
-                //to the next book. In a real app, we'd need better error handling.
+                // In this context, most errors will be 404s that mean
+                // we've run out of lines in the book, so we'll trigger a switch
+                // to the next book. In a real app, we'd need better error handling.
                 console.log('Changing books...');
                 // There are only 12 books in the Aeneid, so...
                 if (book_counter <= 11) {
                     book_counter +=1;
                 }
                 else {
-                    //Go back to first book if we've finished 12
+                    // Go back to first book if we've finished 12
                     book_counter = 1;
                 }
                 line_counter = 0;
@@ -94,7 +94,9 @@ function makeVergilTweet() {
 // Try to tweet something as soon as we run the program...
 makeVergilTweet();
 
-// ...and then every 2 seconds after that. Time here is in milliseconds, so
-// 1000 ms = 1 second. The rate limit for the Aeneid API is 1 call per user
-// per second, there are two calls made per Tweet, so the base unit should be 2000.
-setInterval(makeVergilTweet, 2000);
+// ...and then every 50 minutes after that. The rate is in milliseconds, so
+// 1000 ms = 1 second times 60 = 1 minute times 50 = 50 minutes.
+// Note: The rate limit for the Aeneid API is 1 call per user
+// per second, there are two calls made per Tweet, so there should never be
+// more than one Tweet per two seconds.
+setInterval(makeVergilTweet, 1000 * 60 * 50);
