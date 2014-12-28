c771a4af2f160d2a9298add27f61d4300b4e0e5c
Amanda <amanda.n.krauss@gmail.com>
2014-12-28 09:41:19 -0600

    add error handling; limit rate call per API rules
    Conflicts:
    	bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var T = new Twit(require('./config.js'));
 var LatinURLBase = "http://api.aeneid.eu/versions/latin/";
 var EnglishURLBase = "http://api.aeneid.eu/versions/dryden/";
 
-var book_counter = 0;
+var book_counter = 1;
 var line_counter = 0;
 
 var english = "";
@@ -16,29 +16,38 @@ var latin = "";
 
 function makeVergil() {
     line_counter += 1;
-    // There are only 12 books in the Aeneid
-    if (book_counter <= 11) {
-        book_counter +=1;
-    }
-    else {
-        //reset entirely if we get past the last book
-        book_counter = 1;
-        line_counter = 1;
-    }
-    getLatinURL = LatinURLBase + '/' + book_counter + '/' + line_counter;
-    getEnglishURL = EnglishURLBase + '/' + book_counter + '/' + line_counter;
+    // // There are only 12 books in the Aeneid
+    // if (book_counter <= 11) {
+    //     book_counter +=1;
+    // }
+    // else {
+    //     //reset entirely if we get past the last book
+    //     book_counter = 1;
+    //     line_counter = 1;
+    // }
+    var getLatinURL = LatinURLBase + '/' + book_counter + '/' + line_counter;
+    var getEnglishURL = EnglishURLBase + '/' + book_counter + '/' + line_counter;
 	request(getLatinURL,
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
     	       latin = (data.text);
-  		}
+  		    }
+            else {
+                console.log('Status Code: ' + response.statusCode);
+                var error_msg = (JSON.parse(body)).error;
+                console.log('Error:' + error_msg);
+            }
         request(getEnglishURL,
             function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                 var data = JSON.parse(body);
                 english = (data.text);
-                // console.log(english);
+            }
+            else {
+                console.log('Status Code: ' + response.statusCode);
+                var error_msg = (JSON.parse(body)).error;
+                console.log('Error:' + error_msg);
             }
         statement = latin + '/ ' + english;
         console.log('book ' + book_counter + ', line ' + line_counter);
@@ -49,6 +58,8 @@ function makeVergil() {
 
 // Try to retweet something as soon as we run the program...
 makeVergil();
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(makeVergil(), 1000 * 60 * 60);
+
+// ...and then every 2 seconds after that. Time here is in milliseconds, so
+// 1000 ms = 1 second. The rate limit for the Aeneid API is 1 call per user
+// per second, there are two calls made per Tweet, so the base unit should be 2000.
+setInterval(makeVergil, 2000);
